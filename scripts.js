57bec06ec058c9165013d0e20ada8ae06c82cc6b
Chase Clark <chase_clark07@hotmail.com>
2016-12-05 03:21:05 -0500

    added canvas and jquery scripts

--- scripts.js
+++ scripts.js
@@ -1,8 +1,21 @@
+$(document).ready(function () {
+
+    $("h1,h3").fadeIn(1200);
+
+
+   
+});
+
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+ctx.font = "20px Arial"
+ctx.fillText("Like us on Facebook!",10,50);
+
 function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
     }
-}
